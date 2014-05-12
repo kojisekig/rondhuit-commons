8d2b6ddd9c7d86b04c1cb5c6ab635f7a06c30a4b
Koji Sekiguchi <koji@r.email.ne.jp>
2014-05-12 17:28:05 +0900

    fix #15: import some methods from RCSS JapaneseUtil

--- src/java/com/rondhuit/commons/JapaneseUtil.java
+++ src/java/com/rondhuit/commons/JapaneseUtil.java
@@ -119,6 +119,32 @@ public class JapaneseUtil {
   public static boolean isNumber( int c ){
     return isNumber( (char)c );
   }
+
+  public static final char[] KANJI_NUMBERS = {
+    '〇', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '百', '千', '万', '億', '兆', '京',
+    '零', '壱', '弐', '参', '拾', '萬'
+  };
+  
+  /**
+   * 文字が漢数字か否かを判定する。
+   * @param c 判定対象の文字
+   * @return 判定結果。漢数字の時はtrue
+   */
+  public static boolean isKanjiNumber( char c ){
+    for( char k : KANJI_NUMBERS ){
+      if( k == c ) return true;
+    }
+    return false;
+  }
+  
+  /**
+   * 文字が漢数字か否かを判定する。
+   * @param c 判定対象の文字
+   * @return 判定結果。漢数字の時はtrue
+   */
+  public static boolean isKanjiNumber( int c ){
+    return isKanjiNumber( (char)c );
+  }
   
   /**
    * 文字列がすべてアルファベットか否かを判定する。
@@ -261,6 +287,105 @@ public class JapaneseUtil {
   }
   
   /**
+   * 文字列がすべてひらがなか否かを判定する。
+   * 判定にはUTF-16の「ぁ」から「ん」の間の文字か否かで判断する。
+   * @param str 判定対象の文字列
+   * @return 判定結果。すべてひらがなのときはtrue
+   */
+  public static boolean isHiraganaUTF16( String str ){
+    return isHiraganaUTF16( str.toCharArray() );
+  }
+
+  /**
+   * 文字列がすべてひらがなか否かを判定する。
+   * 判定にはUTF-16の「ぁ」から「ん」の間の文字か否かで判断する。
+   * @param str 判定対象の文字列
+   * @return 判定結果。すべてひらがなのときはtrue
+   */
+  public static boolean isHiraganaUTF16( char[] str ){
+    if( str.length == 0 ) return false;
+    for( char c : str ){
+      if( !isHiraganaUTF16( c ) ) return false;
+    }
+    return true;
+  }
+  
+  /**
+   * 文字列がすべてひらがなか否かを判定する。
+   * 判定にはUTF-16の「ぁ」から「ん」の間の文字か否かで判断する。
+   * @param str 判定対象の文字列
+   * @param start チェックする開始位置
+   * @param len チェックする長さ
+   * @return 判定結果。すべてひらがなのときはtrue
+   */
+  public static boolean isHiraganaUTF16( char[] str, int start, int len ){
+    if( str.length < start + len ) return false;
+    for( int i = 0; i < len; i++ ){
+      char c = str[start + i];
+      if( !isHiraganaUTF16( c ) ) return false;
+    }
+    return true;
+  }
+  
+  /**
+   * 文字がひらがなかひらがなでないかを判定する。
+   * 判定にはUTF-16の「ぁ」から「ん」の間の文字か否かで判断する。
+   * @param c 判定対象の文字
+   * @return 判定結果。ひらがなのときはtrue
+   */
+  public static boolean isHiraganaUTF16( char c ){
+    return c >= 'ぁ' && c <= 'ん';
+  }
+  
+  /**
+   * 文字がひらがなかひらがなでないかを判定する。
+   * 判定にはUTF-16の「ぁ」から「ん」の間の文字か否かで判断する。
+   * @param c 判定対象の文字
+   * @return 判定結果。ひらがなのときはtrue
+   */
+  public static boolean isHiraganaUTF16( int c ){
+    return isHiraganaUTF16( (char)c );
+  }
+  
+  /**
+   * 文字列にひらがなが含まれているか否かを判定する。
+   * @param str 判定対象の文字列
+   * @return 判定結果。ひらがなが含まれている時はtrue
+   */
+  public static boolean isHiraganaIncluded( String str ){
+    return isHiraganaIncluded( str.toCharArray() );
+  }
+
+  /**
+   * 文字列にひらがなが含まれているか否かを判定する。
+   * @param str 判定対象の文字列
+   * @return 判定結果。ひらがなが含まれている時はtrue
+   */
+  public static boolean isHiraganaIncluded( char[] str ){
+    if( str.length == 0 ) return false;
+    for( char c : str ){
+      if( isHiragana( c ) ) return true;
+    }
+    return false;
+  }
+  
+  /**
+   * 文字列にひらがなが含まれているか否かを判定する。
+   * @param str 判定対象の文字列
+   * @param start チェックする開始位置
+   * @param len チェックする長さ
+   * @return 判定結果。ひらがなが含まれている時はtrue
+   */
+  public static boolean isHiraganaIncluded( char[] str, int start, int len ){
+    if( str.length < start + len ) return false;
+    for( int i = 0; i < len; i++ ){
+      char c = str[start + i];
+      if( isHiragana( c ) ) return true;
+    }
+    return false;
+  }
+  
+  /**
    * 文字列がすべてカタカナか否かを判定する。なお、カタカナは全角であっても半角であっても良い。
    * @param str 判定対象の文字列
    * @return 判定結果。すべてカタカナの時はtrue
@@ -371,6 +496,176 @@ public class JapaneseUtil {
   }
   
   /**
+   * 文字列がすべて全角カタカナか否かを判定する。
+   * 判定にはUTF-16の「ァ」から「ヶ」の間の文字か否かで判断する。
+   * ひらがなに変換できない「ヴ」「ヵ」「ヶ」を含む。
+   * @param str 判定対象の文字列
+   * @return 判定結果。すべてカタカナの時はtrue
+   */
+  public static boolean isDoubleWidthKatakanaUTF16( String str ){
+    return isDoubleWidthKatakanaUTF16( str.toCharArray() );
+  }
+
+  /**
+   * 文字列がすべて全角カタカナか否かを判定する。
+   * 判定にはUTF-16の「ァ」から「ヶ」の間の文字か否かで判断する。
+   * ひらがなに変換できない「ヴ」「ヵ」「ヶ」を含む。
+   * @param str 判定対象の文字列
+   * @return 判定結果。すべてカタカナの時はtrue
+   */
+  public static boolean isDoubleWidthKatakanaUTF16( char[] str ){
+    if( str.length == 0 ) return false;
+    for( char c : str ){
+      if( !isDoubleWidthKatakanaUTF16( c ) ) return false;
+    }
+    return true;
+  }
+  
+  /**
+   * 文字列がすべて全角カタカナか否かを判定する。
+   * 判定にはUTF-16の「ァ」から「ヶ」の間の文字か否かで判断する。
+   * ひらがなに変換できない「ヴ」「ヵ」「ヶ」を含む。
+   * @param str 判定対象の文字列
+   * @param start チェックする開始位置
+   * @param len チェックする長さ
+   * @return 判定結果。すべてカタカナの時はtrue
+   */
+  public static boolean isDoubleWidthKatakanaUTF16( char[] str, int start, int len ){
+    if( str.length < start + len ) return false;
+    for( int i = 0; i < len; i++ ){
+      char c = str[start + i];
+      if( !isDoubleWidthKatakanaUTF16( c ) ) return false;
+    }
+    return true;
+  }
+  
+  /**
+   * 文字が全角カタカナか全角カタカナでないかを判定する。
+   * 判定にはUTF-16の「ァ」から「ヶ」の間の文字か否かで判断する。
+   * ひらがなに変換できない「ヴ」「ヵ」「ヶ」を含む。
+   * @param c 判定対象の文字
+   * @return 判定結果。カタカナのときはtrue
+   */
+  public static boolean isDoubleWidthKatakanaUTF16( char c ){
+    return c >= 'ァ' && c <= 'ヶ';
+  }
+  
+  /**
+   * 文字が全角カタカナか全角カタカナでないかを判定する。
+   * 判定にはUTF-16の「ァ」から「ヶ」の間の文字か否かで判断する。
+   * ひらがなに変換できない「ヴ」「ヵ」「ヶ」を含む。
+   * @param c 判定対象の文字
+   * @return 判定結果。カタカナのときはtrue
+   */
+  public static boolean isDoubleWidthKatakanaUTF16( int c ){
+    return isDoubleWidthKatakanaUTF16( (char)c );
+  }
+  
+  /**
+   * 文字列がすべて全角カタカナか否かを判定する。
+   * 判定にはUTF-16の「ァ」から「ン」の間の文字か否かで判断する。
+   * ひらがなに変換できない「ヴ」「ヵ」「ヶ」は含まない。
+   * @param str 判定対象の文字列
+   * @return 判定結果。すべてカタカナの時はtrue
+   */
+  public static boolean isDoubleWidthKatakanaMappableHiraganaUTF16( String str ){
+    return isDoubleWidthKatakanaMappableHiraganaUTF16( str.toCharArray() );
+  }
+
+  /**
+   * 文字列がすべて全角カタカナか否かを判定する。
+   * 判定にはUTF-16の「ァ」から「ン」の間の文字か否かで判断する。
+   * ひらがなに変換できない「ヴ」「ヵ」「ヶ」は含まない。
+   * @param str 判定対象の文字列
+   * @return 判定結果。すべてカタカナの時はtrue
+   */
+  public static boolean isDoubleWidthKatakanaMappableHiraganaUTF16( char[] str ){
+    if( str.length == 0 ) return false;
+    for( char c : str ){
+      if( !isDoubleWidthKatakanaMappableHiraganaUTF16( c ) ) return false;
+    }
+    return true;
+  }
+  
+  /**
+   * 文字列がすべて全角カタカナか否かを判定する。
+   * 判定にはUTF-16の「ァ」から「ン」の間の文字か否かで判断する。
+   * ひらがなに変換できない「ヴ」「ヵ」「ヶ」は含まない。
+   * @param str 判定対象の文字列
+   * @param start チェックする開始位置
+   * @param len チェックする長さ
+   * @return 判定結果。すべてカタカナの時はtrue
+   */
+  public static boolean isDoubleWidthKatakanaMappableHiraganaUTF16( char[] str, int start, int len ){
+    if( str.length < start + len ) return false;
+    for( int i = 0; i < len; i++ ){
+      char c = str[start + i];
+      if( !isDoubleWidthKatakanaMappableHiraganaUTF16( c ) ) return false;
+    }
+    return true;
+  }
+  
+  /**
+   * 文字が全角カタカナか全角カタカナでないかを判定する。
+   * 判定にはUTF-16の「ァ」から「ン」の間の文字か否かで判断する。
+   * ひらがなに変換できない「ヴ」「ヵ」「ヶ」は含まない。
+   * @param c 判定対象の文字
+   * @return 判定結果。カタカナのときはtrue
+   */
+  public static boolean isDoubleWidthKatakanaMappableHiraganaUTF16( char c ){
+    return c >= 'ァ' && c <= 'ン';
+  }
+  
+  /**
+   * 文字が全角カタカナか全角カタカナでないかを判定する。
+   * 判定にはUTF-16の「ァ」から「ン」の間の文字か否かで判断する。
+   * ひらがなに変換できない「ヴ」「ヵ」「ヶ」は含まない。
+   * @param c 判定対象の文字
+   * @return 判定結果。カタカナのときはtrue
+   */
+  public static boolean isDoubleWidthKatakanaMappableHiraganaUTF16( int c ){
+    return isDoubleWidthKatakanaMappableHiraganaUTF16( (char)c );
+  }
+  
+  /**
+   * 文字列にカタカナが含まれているか否かを判定する。
+   * @param str 判定対象の文字列
+   * @return 判定結果。カタカナが含まれている時はtrue
+   */
+  public static boolean isKatakanaIncluded( String str ){
+    return isKatakanaIncluded( str.toCharArray() );
+  }
+
+  /**
+   * 文字列にカタカナが含まれているか否かを判定する。
+   * @param str 判定対象の文字列
+   * @return 判定結果。カタカナが含まれている時はtrue
+   */
+  public static boolean isKatakanaIncluded( char[] str ){
+    if( str.length == 0 ) return false;
+    for( char c : str ){
+      if( isKatakana( c ) ) return true;
+    }
+    return false;
+  }
+  
+  /**
+   * 文字列にカタカナが含まれているか否かを判定する。
+   * @param str 判定対象の文字列
+   * @param start チェックする開始位置
+   * @param len チェックする長さ
+   * @return 判定結果。カタカナが含まれている時はtrue
+   */
+  public static boolean isKatakanaIncluded( char[] str, int start, int len ){
+    if( str.length < start + len ) return false;
+    for( int i = 0; i < len; i++ ){
+      char c = str[start + i];
+      if( isKatakana( c ) ) return true;
+    }
+    return false;
+  }
+  
+  /**
    * 文字列がすべて漢字か否かを判定する。
    * @param str 判定対象の文字列
    * @return 判定結果。すべて漢字の時はtrue
@@ -431,5 +726,137 @@ public class JapaneseUtil {
    */
   public static boolean isKanji( int c ){
     return isKanji( (char)c );
+  }
+
+  private static final int deltaH2K = 'ァ' - 'ぁ';
+  private static final int deltaK2H = 'ぁ' - 'ァ';
+  
+  /**
+   * ひらがな（UTF-16の「ぁ」から「ん」）を全角カタカナ（UTF-16の「ァ」から「ン」）に変換する。
+   * @param hiragana 変換対象のひらがな文字列
+   * @return 変換後のカタカナ文字列
+   */
+  public static String transHiraganaDoubleWidthKatakanaUTF16( String hiragana ){
+    char[] str = new char[hiragana.length()];
+    for( int i = 0; i < str.length; i++ ){
+      str[i] = hiragana.charAt( i );
+      if( isHiraganaUTF16( str[i] ) )
+        str[i] = (char)( str[i] + deltaH2K );
+    }
+    return new String( str );
+  }
+  
+  /**
+   * 全角カタカナ（UTF-16の「ァ」から「ン」）をひらがな（UTF-16の「ぁ」から「ん」）に変換する。
+   * @param katakana 変換対象のカタカナ文字列
+   * @return 変換後のひらがな文字列
+   */
+  public static String transDoubleWidthKatakanaHiraganaUTF16( String katakana ){
+    char[] str = new char[katakana.length()];
+    for( int i = 0; i < str.length; i++ ){
+      str[i] = katakana.charAt( i );
+      if( isDoubleWidthKatakanaMappableHiraganaUTF16( str[i] ) )
+        str[i] = (char)( str[i] + deltaK2H );
+    }
+    return new String( str );
+  }
+  
+  static final char[] TURBIDABLE_HIRAGANA = { 'か','き','く','け','こ','さ','し','す','せ','そ','た','ち','つ','て','と','は','ひ','ふ','へ','ほ' };
+  static final char[] TURBID_HIRAGANA = { 'が','ぎ','ぐ','げ','ご','ざ','じ','ず','ぜ','ぞ','だ','ぢ','づ','で','ど','ば','び','ぶ','べ','ぼ' };
+
+  /**
+   * ひらがなに濁点を付ける。
+   * @param c 濁点を付ける対象のひらがな
+   * @return 濁点が付けられたひらがな
+   */
+  public static char turbidHiragana( char c ){
+    for( int i = 0; i < TURBIDABLE_HIRAGANA.length; i++ ){
+      if( c == TURBIDABLE_HIRAGANA[i] )
+        return TURBID_HIRAGANA[i];
+    }
+    return c;
+  }
+
+  /**
+   * ひらがなから濁点を取り除く。
+   * @param c 濁点が付いたひらがな
+   * @return 濁点がとれたひらがな
+   */
+  public static char unturbidHiragana( char c ){
+    for( int i = 0; i < TURBID_HIRAGANA.length; i++ ){
+      if( c == TURBID_HIRAGANA[i] )
+        return TURBIDABLE_HIRAGANA[i];
+    }
+    return c;
+  }
+  
+  static final char[] TURBIDABLE_KATAKANA = { 'カ','キ','ク','ケ','コ','サ','シ','ス','セ','ソ','タ','チ','ツ','テ','ト','ハ','ヒ','フ','ヘ','ホ' };
+  static final char[] TURBID_KATAKANA = { 'ガ','ギ','グ','ゲ','ゴ','ザ','ジ','ズ','ゼ','ゾ','ダ','ヂ','ヅ','デ','ド','バ','ビ','ブ','ベ','ボ' };
+  
+  /**
+   * カタカナに濁点を付ける。
+   * @param c 濁点を付ける対象のカタカナ
+   * @return 濁点が付けられたカタカナ
+   */
+  public static char turbidKatakana( char c ){
+    for( int i = 0; i < TURBIDABLE_KATAKANA.length; i++ ){
+      if( c == TURBIDABLE_KATAKANA[i] )
+        return TURBID_KATAKANA[i];
+    }
+    return c;
+  }
+
+  /**
+   * カタカナから濁点を取り除く。
+   * @param c 濁点が付いたカタカナ
+   * @return 濁点がとれたカタカナ
+   */
+  public static char unturbidKatakana( char c ){
+    for( int i = 0; i < TURBID_KATAKANA.length; i++ ){
+      if( c == TURBID_KATAKANA[i] )
+        return TURBIDABLE_KATAKANA[i];
+    }
+    return c;
+  }
+
+  /**
+   * ひらがなまたはカタカナに濁点を付ける。
+   * @param c 濁点を付ける対象のひらがなまたはカタカナ
+   * @return 濁点が付けられたひらがなまたはカタカナ
+   */
+  public static char turbidKana( char c ){
+    if( isHiragana( c ) ) return turbidHiragana( c );
+    else if( isKatakana( c ) ) return turbidKatakana( c );
+    return c;
+  }
+
+  /**
+   * ひらがなまたはカタカナから濁点を取り除く。
+   * @param c 濁点が付いたひらがなまたはカタカナ
+   * @return 濁点がとれたひらがなまたはカタカナ
+   */
+  public static char unturbidKana( char c ){
+    if( isHiragana( c ) ) return unturbidHiragana( c );
+    else if( isKatakana( c ) ) return unturbidKatakana( c );
+    return c;
+  }
+  
+  /**
+   * 文字が長音記号か否かを判定する。
+   * @param c 判定する文字
+   * @return 長音記号の場合はtrue。長音記号でないときはfalse
+   */
+  public static boolean isProlongedSoundMark( char c ){
+    return c == KATAKANA_HIRAGANA_PROLONGED_SOUND_MARK ||
+      c == HALF_KATAKANA_PROLONGED_SOUND_MARK;
+  }
+  
+  /**
+   * 文字が長音記号か否かを判定する。
+   * @param c 判定する文字
+   * @return 長音記号の場合はtrue。長音記号でないときはfalse
+   */
+  public static boolean isProlongedSoundMark( int c ){
+    return isProlongedSoundMark( (char)c );
   }
 }
--- src/test/com/rondhuit/commons/JapaneseUtilTest.java
+++ src/test/com/rondhuit/commons/JapaneseUtilTest.java
@@ -14,6 +14,27 @@ import org.junit.Test;
 public class JapaneseUtilTest {
 
   @Test
+  public void testH2KUTF16() throws Exception {
+    assertEquals( "ホンジツハセイテンナリ。", JapaneseUtil.transHiraganaDoubleWidthKatakanaUTF16( "ほんじつはせいてんなり。" ) );
+    assertEquals( "バイナリファイル", JapaneseUtil.transHiraganaDoubleWidthKatakanaUTF16( "ばいなりファイル" ) );
+    assertEquals( "本日ハ晴天ナリ。", JapaneseUtil.transHiraganaDoubleWidthKatakanaUTF16( "本日は晴天なり。" ) );
+    assertEquals( "オープン・ソース", JapaneseUtil.transHiraganaDoubleWidthKatakanaUTF16( "おーぷん・そーす" ) );
+    assertEquals( "オオプン・ソオス", JapaneseUtil.transHiraganaDoubleWidthKatakanaUTF16( "おおぷん・そおす" ) );
+    assertEquals( "全漢字", JapaneseUtil.transHiraganaDoubleWidthKatakanaUTF16( "全漢字" ) );
+    assertEquals( "ヴヵヶ", JapaneseUtil.transHiraganaDoubleWidthKatakanaUTF16( "ヴヵヶ" ) );
+  }
+
+  @Test
+  public void testK2HUTF16() throws Exception {
+    assertEquals( "ほんじつはせいてんなり。", JapaneseUtil.transDoubleWidthKatakanaHiraganaUTF16( "ホンジツハセイテンナリ。" ) );
+    assertEquals( "ばいなりふぁいる", JapaneseUtil.transDoubleWidthKatakanaHiraganaUTF16( "ばいなりファイル" ) );
+    assertEquals( "本日は晴天なり。", JapaneseUtil.transDoubleWidthKatakanaHiraganaUTF16( "本日ハ晴天ナリ。" ) );
+    assertEquals( "おーぷん・そーす", JapaneseUtil.transDoubleWidthKatakanaHiraganaUTF16( "オープン・ソース" ) );
+    assertEquals( "全漢字", JapaneseUtil.transDoubleWidthKatakanaHiraganaUTF16( "全漢字" ) );
+    assertEquals( "ヴヵヶ", JapaneseUtil.transDoubleWidthKatakanaHiraganaUTF16( "ヴヵヶ" ) );
+  }
+
+  @Test
   public void testIsAlphabetAlphabet() throws Exception {
     String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" +
       "ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ" +
@@ -120,6 +141,28 @@ public class JapaneseUtilTest {
       assertEquals( "error at " + str.charAt( i ), expected, JapaneseUtil.isHiragana( str.charAt( i ) ) );
     }
   }
+
+  @Test
+  public void testIsHiraganaUTF16Hiragana() throws Exception {
+    String str = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん" +
+      "がぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽ" +
+      "ぁぃぅぇぉゃゅょ";
+    bulkTestForIsHiraganaUTF16( str, true );
+  }
+
+  @Test
+  public void testIsHiraganaUTF16Katakana() throws Exception {
+    String str = "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン" +
+      "ガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポ" +
+      "ァィゥェォャュョ";
+    bulkTestForIsHiraganaUTF16( str, false );
+  }
+  
+  private void bulkTestForIsHiraganaUTF16( String str, boolean expected ){
+    for( int i = 0; i < str.length(); i++ ){
+      assertEquals( "error at " + str.charAt( i ), expected, JapaneseUtil.isHiraganaUTF16( str.charAt( i ) ) );
+    }
+  }
   
   @Test
   public void testIsHiraganaStr() throws Exception {
@@ -127,6 +170,15 @@ public class JapaneseUtilTest {
     assertTrue( JapaneseUtil.isHiragana( "ひらがな" ) );
     assertFalse( JapaneseUtil.isHiragana( "ひらがな漢字です" ) );
     assertTrue( JapaneseUtil.isHiragana( "ひらがな漢字です".toCharArray(), 0, 4 ) );
+  }
+  
+  @Test
+  public void testIsHiraganaIncluded() throws Exception {
+    assertFalse( JapaneseUtil.isHiraganaIncluded( "" ) );
+    assertTrue( JapaneseUtil.isHiraganaIncluded( "ひらがな" ) );
+    final String s = "ひらがな漢字です";
+    assertTrue( JapaneseUtil.isHiraganaIncluded( s ) );
+    assertTrue( JapaneseUtil.isHiraganaIncluded( s.toCharArray(), 0, s.length() ) );
   }
 
   @Test
@@ -218,6 +270,73 @@ public class JapaneseUtilTest {
       assertEquals( "error at " + str.charAt( i ), expected, JapaneseUtil.isKatakana( str.charAt( i ) ) );
     }
   }
+
+  @Test
+  public void testIsDoubleWidthKatakanaUTF16Hiragana() throws Exception {
+    String str = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん" +
+      "がぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽ" +
+      "ぁぃぅぇぉゃゅょ";
+    bulkTestForIsDoubleWidthKatakanaUTF16( str, false );
+  }
+
+  @Test
+  public void testIsDoubleWidthKatakanaUTF16DoubleWidthKatakana() throws Exception {
+    String str = "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン" +
+      "ガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポ" +
+      "ァィゥェォャュョ" +
+      "ヴヵヶ";
+    bulkTestForIsDoubleWidthKatakanaUTF16( str, true );
+  }
+
+  @Test
+  public void testIsDoubleWidthKatakanaUTF16HalfWidthKatakana() throws Exception {
+    String str = "ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝ" +
+      "ｶﾞｷﾞｸﾞｹﾞｺﾞｻﾞｼﾞｽﾞｾﾞｿﾞﾀﾞﾁﾞﾂﾞﾃﾞﾄﾞﾊﾞﾋﾞﾌﾞﾍﾞﾎﾞﾊﾟﾋﾟﾌﾟﾍﾟﾎﾟ" +
+      "ｧｨｩｪｫｬｭｮ";
+    bulkTestForIsDoubleWidthKatakanaUTF16( str, false );
+  }
+  
+  private void bulkTestForIsDoubleWidthKatakanaUTF16( String str, boolean expected ){
+    for( int i = 0; i < str.length(); i++ ){
+      assertEquals( "error at " + str.charAt( i ), expected, JapaneseUtil.isDoubleWidthKatakanaUTF16( str.charAt( i ) ) );
+    }
+  }
+
+  @Test
+  public void testIsDoubleWidthKatakanaMappableHiraganaUTF16Hiragana() throws Exception {
+    String str = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん" +
+      "がぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽ" +
+      "ぁぃぅぇぉゃゅょ";
+    bulkTestForIsDoubleWidthKatakanaMappableHiraganaUTF16( str, false );
+  }
+
+  @Test
+  public void testIsDoubleWidthKatakanaMappableHiraganaUTF16DoubleWidthKatakanaMappableHiragana() throws Exception {
+    String str = "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン" +
+      "ガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポ" +
+      "ァィゥェォャュョ";
+    bulkTestForIsDoubleWidthKatakanaMappableHiraganaUTF16( str, true );
+  }
+
+  @Test
+  public void testIsDoubleWidthKatakanaMappableHiraganaUTF16DoubleWidthKatakanaUnMappableHiragana() throws Exception {
+    String str = "ヴヵヶ";
+    bulkTestForIsDoubleWidthKatakanaMappableHiraganaUTF16( str, false );
+  }
+
+  @Test
+  public void testIsDoubleWidthKatakanaMappableHiraganaUTF16HalfWidthKatakana() throws Exception {
+    String str = "ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝ" +
+      "ｶﾞｷﾞｸﾞｹﾞｺﾞｻﾞｼﾞｽﾞｾﾞｿﾞﾀﾞﾁﾞﾂﾞﾃﾞﾄﾞﾊﾞﾋﾞﾌﾞﾍﾞﾎﾞﾊﾟﾋﾟﾌﾟﾍﾟﾎﾟ" +
+      "ｧｨｩｪｫｬｭｮ";
+    bulkTestForIsDoubleWidthKatakanaMappableHiraganaUTF16( str, false );
+  }
+  
+  private void bulkTestForIsDoubleWidthKatakanaMappableHiraganaUTF16( String str, boolean expected ){
+    for( int i = 0; i < str.length(); i++ ){
+      assertEquals( "error at " + str.charAt( i ), expected, JapaneseUtil.isDoubleWidthKatakanaMappableHiraganaUTF16( str.charAt( i ) ) );
+    }
+  }
   
   @Test
   public void testIsKatakanaStr() throws Exception {
@@ -225,6 +344,72 @@ public class JapaneseUtilTest {
     assertTrue( JapaneseUtil.isKatakana( "カタカナ" ) );
     assertFalse( JapaneseUtil.isKatakana( "カタカナです" ) );
     assertTrue( JapaneseUtil.isKatakana( "カタカナです".toCharArray(), 0, 4 ) );
+  }
+  
+  @Test
+  public void testIsKatakanaIncluded() throws Exception {
+    assertFalse( JapaneseUtil.isKatakanaIncluded( "" ) );
+    assertTrue( JapaneseUtil.isKatakanaIncluded( "カタカナ" ) );
+    final String s = "カタカナです";
+    assertTrue( JapaneseUtil.isKatakanaIncluded( s ) );
+    assertTrue( JapaneseUtil.isKatakanaIncluded( s.toCharArray(), 0, s.length() ) );
+  }
+  
+  @Test
+  public void testIsKanjiHiragana() throws Exception {
+    String str = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん" +
+      "がぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽ";
+    bulkTestForIsKanji( str, false );
+  }
+
+  @Test
+  public void testIsKanjiKatakana() throws Exception {
+    String str = "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン" +
+      "ガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポ";
+    bulkTestForIsKanji( str, false );
+  }
+
+  @Test
+  public void testIsKanjiKanjiSymbols() throws Exception {
+    String str = "々㈠㈡㈢㈣㈤㈥㈦㈧㈨㈩㈪㈫㈬㈭㈮㈯㈰㈱㈲㈳㈴㈵㈶㈷㈸㈹㈺㈻㈼㈽㈾㈿";
+    bulkTestForIsKanji( str, false );
+  }
+
+  @Test
+  public void testIsKanjiKanaSymbols() throws Exception {
+    String str = "ゝゞヽヾ";
+    bulkTestForIsKanji( str, false );
+  }
+
+  @Test
+  public void testIsKanjiKanjiRange() throws Exception {
+    for( char c = 0x3400; c < 0x4dc0; c++ )
+      assertTrue( "error at " + Integer.toHexString( c ), JapaneseUtil.isKanji( c ) );
+    for( char c = 0x4e00; c < 0xa000; c++ )
+      assertTrue( "error at " + Integer.toHexString( c ), JapaneseUtil.isKanji( c ) );
+  }
+  
+  private void bulkTestForIsKanji( String str, boolean expected ){
+    for( int i = 0; i < str.length(); i++ ){
+      assertEquals( "error at " + str.charAt( i ), expected, JapaneseUtil.isKanji( str.charAt( i ) ) );
+    }
+  }
+  
+  @Test
+  public void testIsKanjiStr() throws Exception {
+    assertFalse( JapaneseUtil.isKanji( "" ) );
+    assertTrue( JapaneseUtil.isKanji( "漢字" ) );
+    assertFalse( JapaneseUtil.isKanji( "漢字です" ) );
+  }
+  
+  @Test
+  public void testIsKanjiNumber() throws Exception {
+    for( char c : JapaneseUtil.KANJI_NUMBERS ){
+      assertTrue( JapaneseUtil.isKanjiNumber( c ) );
+    }
+    assertFalse( JapaneseUtil.isKanjiNumber( '市' ) );
+    assertFalse( JapaneseUtil.isKanjiNumber( '荷' ) );
+    assertFalse( JapaneseUtil.isKanjiNumber( '賛' ) );
   }
 
   @Test
@@ -235,5 +420,41 @@ public class JapaneseUtilTest {
     assertFalse( JapaneseUtil.isKatakana( JapaneseUtil.KATAKANA_HIRAGANA_PROLONGED_SOUND_MARK, false ) );
     assertTrue( JapaneseUtil.isKatakana( JapaneseUtil.HALF_KATAKANA_PROLONGED_SOUND_MARK ) );
     assertFalse( JapaneseUtil.isKatakana( JapaneseUtil.HALF_KATAKANA_PROLONGED_SOUND_MARK, false ) );
+  }
+  
+  @Test
+  public void testTurbidHiragana() throws Exception {
+    String source = "あいうえおかきくけこ";
+    bulkTestTurbidHiragana( source, "あいうえおがぎぐげご" );
+  }
+  
+  private void bulkTestTurbidHiragana( String source, String expected ){
+    for( int i = 0; i < source.length(); i++ ){
+      assertEquals( expected.charAt( i ), JapaneseUtil.turbidHiragana( source.charAt( i ) ) );
+    }
+  }
+  
+  @Test
+  public void testTurbidKatakana() throws Exception {
+    String source = "カキクケコサシスセソナニヌネノ";
+    bulkTestTurbidKatakana( source, "ガギグゲゴザジズゼゾナニヌネノ" );
+  }
+  
+  private void bulkTestTurbidKatakana( String source, String expected ){
+    for( int i = 0; i < source.length(); i++ ){
+      assertEquals( expected.charAt( i ), JapaneseUtil.turbidKatakana( source.charAt( i ) ) );
+    }
+  }
+  
+  @Test
+  public void testTurbidKana() throws Exception {
+    String source = "あいうえおカキクケコサシスセソナニヌネノはひふへほ";
+    bulkTestTurbidKana( source, "あいうえおガギグゲゴザジズゼゾナニヌネノばびぶべぼ" );
+  }
+  
+  private void bulkTestTurbidKana( String source, String expected ){
+    for( int i = 0; i < source.length(); i++ ){
+      assertEquals( expected.charAt( i ), JapaneseUtil.turbidKana( source.charAt( i ) ) );
+    }
   }
 }
